71d49eed04100586b783be7e6d6e76cf663786c9
malu <malu@malu.me>
2019-03-20 16:27:20 +0800

    add elui

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div id="app">
     <img src="https://ws1.sinaimg.cn/large/a83bb572gy1g18125erxug20go0971df.gif">
-    <router-view/>
+    <div>
+      <p>
+        If Element is successfully added to this project, you'll see an
+        <code v-text="'<el-button>'"></code>
+        below
+      </p>
+      <el-button>el-button</el-button>
+    </div>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import HelloWorld from './components/HelloWorld.vue'
+
 export default {
-  name: 'App'
+  name: 'app',
+  components: {
+    HelloWorld
+  }
 }
 </script>
 
