8e6dacb464691cf9801630f48231c633939890e4
malu <malu@malu.me>
2019-03-26 18:02:26 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -6,20 +6,41 @@ import Mshow from '@/components/m-show'
 
 Vue.use(VueRouter)
 
-export default new VueRouter({
+const router = new VueRouter({
   mode: 'history',
   base: '/vue/',
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
-      component: HelloWorld
+      component: HelloWorld,
+      meta: {
+        title: '首页'
+      }
     },
     {
       path: '/show',
       name: 'Mshow',
-      component: Mshow
+      component: Mshow,
+      meta: {
+        title: '项目'
+      }
     },
-    { path: '*', component: NotFoundComponent }
+    {
+      path: '*',
+      component: NotFoundComponent,
+      meta: {
+        title: '404 找不到页面'
+      }
+    }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) { // 如果设置标题，拦截后设置标题
+    document.title = to.meta.title
+  }
+  next()
+})
+
+export default router
