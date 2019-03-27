8f8c4a7245a13d6e835e2abaa0d84667c1cfc098
malu <malu@malu.me>
2019-03-27 18:04:12 +0800

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,25 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
 
-    <h2>{{ list }}</h2>
+    <div>
+      <h1>Bitcoin Price Index</h1>
+
+      <section v-if="errored">
+        <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
+      </section>
+
+      <section v-else>
+        <div v-if="loading">Loading...</div>
+
+        <div v-else v-for="currency in info" :key="currency" class="currency">
+          {{ currency.description }}:
+          <span class="lighten">
+            <span v-html="currency.symbol"></span>
+            {{ currency.rate_float | currencydecimal }}
+          </span>
+        </div>
+      </section>
+    </div>
     <ul>
       <li>
         <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
@@ -21,20 +39,34 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 
 export default {
   name: "HelloWorld",
   data() {
     return {
-      list: null,
+      info: null,
+      loading: true,
+      errored: false,
       msg: "Welcome to Your Vue.js App"
     };
   },
-    mounted () {
+  filters: {
+    currencydecimal(value) {
+      return value.toFixed(2);
+    }
+  },
+  mounted() {
     axios
-      .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-      .then(response => (this.list = response))
+      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(response => {
+        this.info = response.data.bpi;
+      })
+      .catch(error => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   }
 };
 </script>
