37680166bade2a940729a71735c0365b62c05719
malu <malu@malu.me>
2019-03-29 18:04:14 +0800

    thres

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,104 +1,61 @@
 <template>
-  <div class="hello">
-    <div id="bitcoin">
-      <h1>实时比特币价格指数</h1>
-
-      <section v-if="errored">
-        <p>We're sorry, we're not able to retrieve this information at the moment, please try back later</p>
-      </section>
-
-      <section v-else>
-        <div v-if="loading">Loading...</div>
-
-        <div v-else v-for="currency in info" :key="currency.value" class="currency">
-          {{ currency.code | tochinese }}:
-          <span class="lighten">{{ currency.rate_float | currencydecimal }}</span>
-        </div>
-      </section>
-    </div>
-    <!-- <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">vue-router</a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">vuex</a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a>
-      </li>
-    </ul> -->
-  </div>
+  <div id="containerdd"></div>
 </template>
 
 <script>
-import axios from "axios";
+// import Three from '../plugins/three.min.js'
+import * as Three from 'three'
 
 export default {
-  name: "HelloWorld",
-  data() {
+  name: 'Home',
+  data () {
     return {
-      info: null,
-      loading: true,
-      errored: false,
-      msg: "Welcome to Your Vue.js App"
-    };
-  },
-  filters: {
-    currencydecimal(value) {
-      return value.toFixed(2);
-    },
-    tochinese(value) {
-      switch (value) {
-        case "USD":
-          return "美元";
-        case "GBP":
-          return "英镑";
-        case "EUR":
-          return "欧元";
-      }
+      camera: null,
+      scene: null,
+      renderer: null,
+      mesh: null
     }
   },
-  mounted() {
-    axios
-      .get("https://api.coindesk.com/v1/bpi/currentprice.json")
-      .then(response => {
-        this.info = response.data.bpi;
-      })
-      .catch(error => {
-        console.log(error);
-        this.errored = true;
-      })
-      .finally(() => (this.loading = false));
-  }
-};
-</script>
+  methods: {
+    init: function () {
+      var container = document.getElementById('containerdd')
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1,
-h2 {
-  font-weight: normal;
+      this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10)
+      this.camera.position.z = 1
+
+      this.scene = new Three.Scene()
+
+      var geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
+      var material = new Three.MeshNormalMaterial()
+
+      this.mesh = new Three.Mesh(geometry, material)
+      this.scene.add(this.mesh)
+
+      this.renderer = new Three.WebGLRenderer({ antialias: true })
+      this.renderer.setSize(container.clientWidth, container.clientHeight)
+      container.appendChild(this.renderer.domElement)
+    },
+    animate: function () {
+      requestAnimationFrame(this.animate)
+      this.mesh.rotation.x += 0.01
+      this.mesh.rotation.y += 0.02
+      this.renderer.render(this.scene, this.camera)
+    }
+  },
+  mounted () {
+    this.init()
+    this.animate()
+  }
 }
-ul {
-  list-style-type: none;
+
+</script>
+<style  scoped>
+#container {
+  width: 100%;
+  height: 100%;
+  margin: 0;
   padding: 0;
+  overflow: hidden;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-#bitcoin{
-      margin-top: 20px;
-    /* width: 300px; */
-    padding: 0 40px 40px;
-    /* background: #2F242C; */
-    border-radius: 5px;
-    color: #B3BFB8;
-}
+
 </style>
