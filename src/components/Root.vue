5d82eb94e623ee90809e80d40680fee43e9f3d5f
malu <malu@malu.me>
2019-04-01 11:17:27 +0800

    Update Root.vue

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,14 +1,15 @@
 <template>
-  <div id="containerdd"></div>
+    <div>
+      <div id="container"></div>
+    </div>
 </template>
-
+ 
 <script>
-// import Three from '../plugins/three.min.js'
 import * as Three from 'three'
-
+ 
 export default {
-  name: 'Home',
-  data () {
+  name: 'ThreeTest',
+  data() {
     return {
       camera: null,
       scene: null,
@@ -17,45 +18,40 @@ export default {
     }
   },
   methods: {
-    init: function () {
-      var container = document.getElementById('containerdd')
-
-      this.camera = new Three.PerspectiveCamera(70, container.clientWidth / container.clientHeight, 0.01, 10)
-      this.camera.position.z = 1
-
-      this.scene = new Three.Scene()
-
-      var geometry = new Three.BoxGeometry(0.2, 0.2, 0.2)
-      var material = new Three.MeshNormalMaterial()
-
-      this.mesh = new Three.Mesh(geometry, material)
-      this.scene.add(this.mesh)
-
-      this.renderer = new Three.WebGLRenderer({ antialias: true })
-      this.renderer.setSize(container.clientWidth, container.clientHeight)
-      container.appendChild(this.renderer.domElement)
+    init: function() {
+        let container = document.getElementById('container');
+ 
+        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+        this.camera.position.z = 1;
+ 
+        this.scene = new Three.Scene();
+ 
+        let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+        let material = new Three.MeshNormalMaterial();
+ 
+        this.mesh = new Three.Mesh(geometry, material);
+        this.scene.add(this.mesh);
+ 
+        this.renderer = new Three.WebGLRenderer({antialias: true});
+        this.renderer.setSize(container.clientWidth, container.clientHeight);
+        container.appendChild(this.renderer.domElement);
+ 
     },
-    animate: function () {
-      requestAnimationFrame(this.animate)
-      this.mesh.rotation.x += 0.01
-      this.mesh.rotation.y += 0.02
-      this.renderer.render(this.scene, this.camera)
+    animate: function() {
+        requestAnimationFrame(this.animate);
+        this.mesh.rotation.x += 0.01;
+        this.mesh.rotation.y += 0.02;
+        this.renderer.render(this.scene, this.camera);
     }
   },
-  mounted () {
-    this.init()
-    this.animate()
+  mounted() {
+      this.init();
+      this.animate()
   }
 }
-
 </script>
-<style  scoped>
-#container {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  overflow: hidden;
-}
-
+<style scoped>
+  #container {
+    height: 400px;
+  }
 </style>
