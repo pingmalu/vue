839aea156e109c5c3920a0aafd616616b28751e2
malu <malu@malu.me>
2019-04-02 09:29:41 +0800

    root2

--- src/components/Root.vue
+++ src/components/Root.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <root1></root1>
+    <root2></root2>
   </div>
 </template>
  
 <script>
 import root1 from "@/components/Root_1";
+import root2 from "@/components/Root_2";
 
 export default {
   components:{
-    root1
+    root1,
+    root2
   }
 };
 </script>
