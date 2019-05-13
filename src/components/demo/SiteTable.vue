db672c2bc28739b5fdba7543ee6823f486e35fbd
malu <malu@malu.me>
2019-05-13 17:36:31 +0800

    d

--- src/components/demo/SiteTable.vue
+++ src/components/demo/SiteTable.vue
@@ -9,19 +9,20 @@
         :data="info.children"
         stripe
         height="430"
+        :row-key="info.children.key"
         style="width: 100%"
+        is="transition-group"
       >
-        <transition-group name="flip-list">
-          <el-table-column key="name" sortable prop="name" label="日期" width="140"></el-table-column>
-          <el-table-column key="value" sortable prop="value" label="事件" width="180" align="center"></el-table-column>
+          <el-table-column sortable prop="name" label="日期" width="140"></el-table-column>
+          <el-table-column sortable prop="value" label="事件" width="180" align="center"></el-table-column>
         </transition-group>
       </el-table> -->
 
       <transition-group tag="tbody" name="flip-list" id="el">
-          <tr v-for="(item,index) in info.children" :key="item.name">
-            <td>{{ item.name }}</td>
-            <td>{{ item.value }}</td>
-          </tr>
+        <tr v-for="(item) in info.children" :key="item.name">
+          <td>{{ item.name }}</td>
+          <td>{{ item.value }}</td>
+        </tr>
       </transition-group>
     </div>
   </div>
@@ -51,26 +52,36 @@ export default {
           {
             name: "blog.malu.me",
             value: "100",
-            label: { color: "rgb(255, 208, 75)" }
+            label: { color: "rgb(255, 208, 75)" },
+            key: 1
           },
           {
             name: "photo.malu.me",
             value: "100",
-            label: { color: "rgb(255, 208, 75)" }
+            label: { color: "rgb(255, 208, 75)" },
+            key: 2
           },
           {
             name: "github.malu.me",
             value: "100",
-            label: { color: "rgb(255, 208, 75)" }
+            label: { color: "rgb(255, 208, 75)" },
+            key: 3
           },
           {
             name: "im.malu.me",
             value: "85.98",
-            label: { color: "rgb(255, 208, 75)" }
+            label: { color: "rgb(255, 208, 75)" },
+            key: 4
           }
         ]
       }
     };
+  },
+  computed: {
+    // 计算属性的 getter
+    radom_num: function() {
+      return Math.random();
+    }
   },
   methods: {
     format_status_data: function() {
