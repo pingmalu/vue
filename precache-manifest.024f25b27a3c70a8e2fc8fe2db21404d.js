b9a2a7fb8633951180982a3b8adacbb523ed730a
malu <malu@malu.me>
2019-05-29 13:49:37 +0800

    Updates

---
+++ precache-manifest.024f25b27a3c70a8e2fc8fe2db21404d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "38169fe5267a463b2b72",
+    "url": "/js/chunk-vendors.d7d097fc.js"
+  },
+  {
+    "revision": "90b1db13b8394e13431d",
+    "url": "/js/app.fad65ae9.js"
+  },
+  {
+    "revision": "5746746b4d9fed780fd5ceda2fa683ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38169fe5267a463b2b72",
+    "url": "/css/chunk-vendors.aae34bb5.css"
+  },
+  {
+    "revision": "90b1db13b8394e13431d",
+    "url": "/css/app.0edf2f69.css"
+  }
+];
