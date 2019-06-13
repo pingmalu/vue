245eb7600ddf47b8952ac02f6a4d6ff91350677a
malu <malu@malu.me>
2019-06-13 17:41:48 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,3 @@
-importScripts("/vue/wb/i-manifest.1f4962000bb9639decc6585f679b57ef.js");
-
 /**
  * Welcome to your Workbox-powered service worker!
  *
@@ -17,10 +15,10 @@ importScripts("/vue/wb/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/vue/wb/workbox-v4.3.1"});
 
 importScripts(
-  "/vue/wb/precache-manifest.870d42406272213b6d5be017dabe759a.js"
+  "/vue/wb/precache-manifest.fce092ef0628a02dc83f39bbc0424349.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "bbq"});
+workbox.core.setCacheNameDetails({prefix: "maluvue"});
 
 workbox.core.skipWaiting();
 
@@ -31,10 +29,26 @@ workbox.core.clientsClaim();
  * requests for URLs in the manifest.
  * See https://goo.gl/S9QRab
  */
-self.__precacheManifest = [].concat(self.__precacheManifest || []);
+self.__precacheManifest = [
+  {
+    "url": "/vue/d1d0",
+    "revision": "6512bd43d9caa6e02c990b0a82652dca"
+  },
+  {
+    "url": "/vue/test",
+    "revision": "6512bd43d9caa6e02c990b0a82652dca"
+  },
+  {
+    "url": "/vue/d4d0",
+    "revision": "6512bd43d9caa6e02c990b0a82652dca"
+  },
+  {
+    "url": "/vue/show",
+    "revision": "6512bd43d9caa6e02c990b0a82652dca"
+  }
+].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerRoute(/vue\/show/, new workbox.strategies.NetworkFirst(), 'GET');
 workbox.routing.registerRoute(/.*\.js/, new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":2, plugins: [] }), 'GET');
-workbox.routing.registerRoute(/.*\.png/, new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":2, plugins: [] }), 'GET');
-
+workbox.routing.registerRoute(/^https:\/\/c1.malu.me\//, new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":2, plugins: [] }), 'GET');
+workbox.routing.registerRoute(/^https:\/\/malu.me\/vue/, new workbox.strategies.NetworkFirst({ "networkTimeoutSeconds":2, plugins: [] }), 'GET');
---
+++ wb/precache-manifest.fce092ef0628a02dc83f39bbc0424349.js
@@ -0,0 +1,89 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5f751687a8072ffbb0ece539c0bb5f3",
+    "url": "/vue/favicon.ico"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/vue/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/vue/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/vue/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/vue/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/vue/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/vue/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/vue/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/vue/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/vue/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/vue/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/vue/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/vue/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "f22d501a35a87d9f21701cb031f6ea17",
+    "url": "/vue/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "7a3f37810218a699217efdadd182c929",
+    "url": "/vue/index.html"
+  },
+  {
+    "revision": "1a8720aad2953fe39e5c331f91b3b49c",
+    "url": "/vue/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue/robots.txt"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/vue/static/fonts/element-icons.535877f.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/vue/static/fonts/element-icons.732389d.ttf"
+  },
+  {
+    "revision": "7fba67b733595399776fb08b28372326",
+    "url": "/vue/static/img/a.7fba67b.png"
+  },
+  {
+    "revision": "38a7243766fc8a0fc40a6d64445e502a",
+    "url": "/vue/static/img/b.38a7243.png"
+  },
+  {
+    "url": "/vue/static/js/app.32419c47619fb7514f60.js"
+  }
+]);
